89431948114bcb38ec64a4c7bec50f5a57b18150
vanessa1270 <fernanda257560@gmail.com>
2026-08-28 11:06:26 -0600

    Semana 3

---
+++ avance_proyecto(Fase1).py
@@ -0,0 +1,24 @@
+#REGISTRO A LA BIENVENIDA DE TECMILENIO
+
+
+print("Bienvenido a la fiesta de bienvenida de Tecmilenio")
+
+nombre = input("\nIngrese tu nombre: ")
+edad = int(input("Ingrese tu edad: "))
+invitado = input("¿Vienes con algún invitado? (si/no): ")
+
+if invitado == "si":
+    nombre_invitado = input("Ingrese el nombre del invitado: ")
+    edad_invitado = int(input("Ingrese la edad del invitado: "))
+else: 
+    nombre_invitado = None
+    edad_invitado = None
+
+if edad < 18 and edad_invitado < 18:
+    print("Pulsera azul para invitado y para alumno tecmilenio")
+elif edad < 18 and edad_invitado >= 18:
+    print("Pulsera roja para invitado y pulsera azul para alumno tecmilenio")
+elif edad >= 18 and edad_invitado < 18:
+    print("Pulsera roja para invitado y pulsera azul para alumno tecmilenio")
+else:
+    print("Pulsera roja para invitado y pulsera roja para alumno tecmilenio")
